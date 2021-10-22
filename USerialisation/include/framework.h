d8ecc77875e3221204c69e96a051f5f7fbb9de42
GrozYeux <66424461+GrozYeux@users.noreply.github.com>
2021-10-22 15:30:54 -0400

    factorisation

--- USerialisation/include/framework.h
+++ USerialisation/include/framework.h
@@ -13,10 +13,21 @@ struct Quaternion
     float w;
 };
 
-inline int maxRange(int min, int max, int prec = 0) {
+inline int maxRange(int min, int max, int prec = 0) 
+{
     return (max - min) * std::pow(10, prec);
 }
 
-inline int nbOctet(int mr) {
+inline int nbOctet(int mr) 
+{
     return ((1 << 8) < mr) + ((1 << 16) < mr) + ((1 << 32) < mr) + ((1 << 64) < mr);
+}
+
+inline int quatOctet(int valx, int valy, int valz, int start, int precision, int min) 
+{
+    return ((start << 30) | 
+        (static_cast<int> ((val.x - min) * precision)) << 20) | 
+        (static_cast<int> ((val.y - min) * precision)) << 10) | 
+        (static_cast<int> ((val.z - min) * precision)))
+    )
 }
--- USerialisation/src/QuatCompressor.cpp
+++ USerialisation/src/QuatCompressor.cpp
@@ -19,21 +19,13 @@ void QuatCompressor::operator()(Serializer s, Quaternion val) override
 	if (max_val == val.w) 
 	{
 		ignore = 0b10;
-		int min = 0;
-		int value_x = static_cast<int> ((val.x - m_min) * precision);
-		int value_y = static_cast<int> ((val.y - m_min) * precision);
-		int value_w = static_cast<int> ((val.w - m_min) * precision);
-		uint32_t res = (ignore << 30) | (value_x << 20) | (value_y << 10) | (value_w);
+		uint32_t res = quatOctet(val.x, val.y, val.w, ignore, m_precision, m_min);
 		s.Serialize(res);
 	}
 	else
 	{
 		ignore = 0b11;
-		int min = 0;
-		int value_x = static_cast<int> ((val.x - m_min) * precision);
-		int value_y = static_cast<int> ((val.y - m_min) * precision);
-		int value_z = static_cast<int> ((val.z - m_min) * precision);
-		uint32_t res = (ignore << 30) | (value_x << 20) | (value_y << 10) | (value_z);
+		uint32_t res = quatOctet(val.x, val.y, val.z, ignore, m_precision, m_min);
 		s.Serialize(res);
 	}
 }

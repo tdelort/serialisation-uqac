a94bb489bc56c0147a02eee89c31169f9407b5f5
GrozYeux <66424461+GrozYeux@users.noreply.github.com>
2021-10-23 18:45:53 -0400

    Update QuatCompressor.cpp

--- USerialisation/src/QuatCompressor.cpp
+++ USerialisation/src/QuatCompressor.cpp
@@ -18,14 +18,26 @@ void QuatCompressor::operator()(Serializer s, Quaternion val) override
 	int ignore;
 	if (max_val == val.w) 
 	{
+		ignore = 0b11;
+		uint32_t res = quatOctet(val.x, val.y, val.z, ignore, m_precision, m_min);
+		s.Serialize(res);
+	}
+	if (max_val == val.z)
+	{
 		ignore = 0b10;
 		uint32_t res = quatOctet(val.x, val.y, val.w, ignore, m_precision, m_min);
 		s.Serialize(res);
 	}
-	else
+	if (max_val == val.y)
 	{
-		ignore = 0b11;
-		uint32_t res = quatOctet(val.x, val.y, val.z, ignore, m_precision, m_min);
+		ignore = 0b01;
+		uint32_t res = quatOctet(val.x, val.w, val.z, ignore, m_precision, m_min);
+		s.Serialize(res);
+	}
+	if (max_val == val.x)
+	{
+		ignore = 0b00;
+		uint32_t res = quatOctet(val.w, val.y, val.z, ignore, m_precision, m_min);
 		s.Serialize(res);
 	}
 }

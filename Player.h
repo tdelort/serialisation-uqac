bba492940fa123cde3498b78338ed027e78b53d6
tdelort <tristan.delort@gmail.com>
2021-10-24 23:50:24 -0400

    player hack fix

--- Player.h
+++ Player.h
@@ -8,11 +8,10 @@
 #include <QuatCompressor.h>
 #include "framework.h"
 
+#include <string>
 
-
-class Player
+struct Player
 {
-private:
     Vector3D m_position; // X Y entre -500 et 500 et Z entre 0 et 100
     Quaternion m_rotation;
     Vector3D m_taille; 
@@ -20,48 +19,49 @@ private:
     int m_armure; // armure entre 0 et 50
     float m_argent; // argent entre -99 999,99 et 99 999,99
     std::string m_nom; // nom de max 128 char
-public:
-    static std::pair<char*,int> Write(Player p)
-    {
-        Serializer s(6);
 
-        IntCompressor vieComp(0, 300);
-        IntCompressor armorComp(0, 50);
-        FloatCompressor moneyComp(-99999.99, 99999.99, 3);
-        VectCompressor positionComp({-500, -500, 0}, {500, 500, 100}, {3, 3, 3});
-        VectCompressor tailleComp({0, 0, 0}, {10, 10, 20}, {3, 3, 3});
-        QuatCompressor rotationComp;
+};
 
-        vieComp.Compress(&s, p.m_vie);
-        armorComp.Compress(&s, p.m_armure);
-        moneyComp.Compress(&s, p.m_argent);
-        positionComp.Compress(&s, p.m_position);
-        tailleComp.Comrpess(&s, p.m_taille);
-        rotationComp.Compress(&s, p.m_rotation);
+std::pair<char*,int> Write(Player p)
+{
+    Serializer s(6);
 
-        return std::make_pair(s.GetBuffer(), s.GetBufferSize());
-    }
+    IntCompressor vieComp(0, 300);
+    IntCompressor armorComp(0, 50);
+    FloatCompressor moneyComp(-99999.99, 99999.99, 3);
+    VectCompressor positionComp({-500, -500, 0}, {500, 500, 100}, {3, 3, 3});
+    VectCompressor tailleComp({0, 0, 0}, {10, 10, 20}, {3, 3, 3});
+    QuatCompressor rotationComp;
 
-    static Player Read(char* buffer, int size)
-    {
-        Desrializer ds(buffer, size);
+    vieComp.Compress(&s, p.m_vie);
+    armorComp.Compress(&s, p.m_armure);
+    moneyComp.Compress(&s, p.m_argent);
+    positionComp.Compress(&s, p.m_position);
+    tailleComp.Compress(&s, p.m_taille);
+    rotationComp.Compress(&s, p.m_rotation);
 
-        IntCompressor vieComp(0, 300);
-        IntCompressor armorComp(0, 50);
-        FloatCompressor moneyComp(-99999.99, 99999.99, 3);
-        VectCompressor positionComp({-500, -500, 0}, {500, 500, 100}, {3, 3, 3});
-        VectCompressor tailleComp({0, 0, 0}, {10, 10, 20}, {3, 3, 3});
-        QuatCompressor rotationComp;
+    return std::make_pair(s.GetBuffer(), s.GetBufferSize());
+}
 
-        Player p;
+Player Read(char* buffer, int size)
+{
+    Deserializer ds(buffer, size);
 
-        p.m_vie = vieComp.Decompress(&ds);
-        p.m_armure = armorComp.Decompress(&ds);
-        p.m_argent = moneyComp.Decompress(&ds);
-        p.m_position = positionComp.Decompress(&ds);
-        p.m_taille = tailleComp.Decomrpess(&ds);
-        p.m_rotation = rotationComp.Decompress(&ds);
+    IntCompressor vieComp(0, 300);
+    IntCompressor armorComp(0, 50);
+    FloatCompressor moneyComp(-99999.99, 99999.99, 3);
+    VectCompressor positionComp({-500, -500, 0}, {500, 500, 100}, {3, 3, 3});
+    VectCompressor tailleComp({0, 0, 0}, {10, 10, 20}, {3, 3, 3});
+    QuatCompressor rotationComp;
 
-        return p;
-    }
+    Player p;
+
+    p.m_vie = vieComp.Decompress(&ds);
+    p.m_armure = armorComp.Decompress(&ds);
+    p.m_argent = moneyComp.Decompress(&ds);
+    p.m_position = positionComp.Decompress(&ds);
+    p.m_taille = tailleComp.Decompress(&ds);
+    p.m_rotation = rotationComp.Decompress(&ds);
+
+    return p;
 }
--- testsPlayer.cpp
+++ testsPlayer.cpp
@@ -1,5 +1,5 @@
 #include <framework.h>
-#include <Player.h>
+#include "Player.h"
 
 #include <iostream>
 
@@ -10,16 +10,16 @@ int main()
     p.m_vie = 300;
     p.m_armure = 0;
     p.m_argent = 100;
-    p.m_position = {10f, 0f, 0f};
-    p.m_taille = {1f, 0.5f, 2f};
+    p.m_position = {10, 0, 0};
+    p.m_taille = {1, 0.5, 2};
     p.m_rotation = {0.4964001f, 0.7791921f, 0.3227519f, -0.2056157f};
 
-    std::pair<char*, int> buffer = Player.Write(p);
+    std::pair<char*, int> buffer = Write(p);
 
-    Player p2 = Player.Read(buffer.first, buffer.second);
+    Player p2 = Read(buffer.first, buffer.second);
     
     std::cout << "vie : " << p.m_vie << std::endl;
-    std::cout << "money : " << p.m_money << std::endl;
+    std::cout << "money : " << p.m_argent << std::endl;
     std::cout << "pos : " << p.m_position.x << " " << p.m_position.y << " " << p.m_position.z << std::endl;
     std::cout << "rot : " << p.m_rotation.x << " " << p.m_rotation.y << " " << p.m_rotation.z << " " << p.m_rotation.w << std::endl;
 
